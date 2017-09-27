51d83737d5ab17ca427dca6afebbbfb97aa657bc
Patrick Steiner <pedisteiner@msn.com>
2017-09-27 10:18:10 +0200

    Google Login

--- 2_Factor_Authentication/2_Factor_Auth/2_Factor_Auth/Controllers/HomeController.cs
+++ 2_Factor_Authentication/2_Factor_Auth/2_Factor_Auth/Controllers/HomeController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+//Client ID: 993337614778-m1jvtg410a9ci2n9pvm7sfnhvk7tmatt.apps.googleusercontent.com
+//Client Secret: BozeenEWPeXPohCojda16mSp
+
 namespace _2_Factor_Auth.Controllers
 {
     public class HomeController : Controller
